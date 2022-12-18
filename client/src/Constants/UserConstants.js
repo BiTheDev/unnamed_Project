5272ae8c5674abc81bad7eb0ddb24d3b151510d9
Brian Ho <brian@MacBook-Pro.local>
2022-12-17 18:38:34 -0800

    front end user constants updated

---
+++ client/src/Constants/UserConstants.js
@@ -0,0 +1,5 @@
+export const LOGIN_USER = 'LOGIN_USER';
+export const CREATE_USER = 'CREATE_USER';
+export const DELETE_USER = 'DELETE_USER';
+export const AUTH = 'AUTH';
+export const LOGOUT = "LOGOUT";
